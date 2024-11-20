c6615236e5218fc2240c81d85515087f4b8d1414
harshalDev <harshalwarade344@gmail.com>
2024-11-20 18:09:41 +0530

    change things

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: { origin: "*" }});
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, { cors: { origin: "*" } });
 const path = require('path');
 const port = 80;
 
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/home', (req, res) => {
-    res.render('home')
-})
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
 
-server.listen(port, (err)=>{
-    console.log('connected')
-})
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+server.listen(port, (err) => {
+    if (err) {
+        console.error('Error starting server:', err);
+    } else {
+        console.log(`Server connected on port ${port}`);
+    }
+});
 
 io.on('connection', (socket) => {
-    console.log("User connected " + socket.id);
+    console.log("User connected: " + socket.id);
 
     socket.on("message", (data) => {
-        // console.log(data    );
-        socket.broadcast.emit('message', data)
-        
-    })
-
-})
+        socket.broadcast.emit('message', data);
+    });
+});
